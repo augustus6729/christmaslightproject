e8134825ee5bca4ae53f885435751c7b243cc17a
austintedders13 <33536411+austintedders13@users.noreply.github.com>
2019-11-18 09:58:58 -0500

    Added a new sound, removed old files

--- keyboardLights.py
+++ keyboardLights.py
@@ -6,24 +6,17 @@ from rpi_ws281x import PixelStrip, Color
 from flask import Flask, render_template, request
 import RPi.GPIO as GPIO
 import urllib
-import webbrowser
+from pygame import mixer
 #import os
 #Setting the backend for the file due to issues installing the default backend
 mido.set_backend('mido.backends.portmidi')
 #for debug purposes print backend on program start
 print(mido.backend)
 print(mido.get_input_names())
-
-# Main program logic follows:
-if __name__ == '__main__':
-    # Process arguments
-   """ parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
-    args = parser.parse_args()"""
+mixer.init()
 
 
 port = mido.open_input('AKM320 MIDI 1')
-print(webbrowser.get())
 msg = port.receive()
 #This loop constantly checks for new messages coming into the port
 for msg in port.__iter__():
@@ -31,25 +24,28 @@ for msg in port.__iter__():
    #in this instance we are checking for the note attribute to assign
    #the note name to the note itself, and only on note_on msg types
     if hasattr(msg, 'note') and msg.note == 53 and msg.type == 'note_on':
+        mixer.music.load('/home/pi/christmaslightproject/TubeBells.wav')
+        mixer.music.play()
         print("Call Rainbow from Flask")
-        #webbrowser.open_new('http://localhost/18/on')
-        #os.system("chromium-browser http://localhost/18/on")
-        urllib.request.urlopen('http://localhost/18/on',data=None)
+        urllib.request.urlopen('http://10.200.2.129/18/on',data=None)
         print(msg)
-    elif hasattr(msg, 'note') and msg.note == 53 and msg.type == 'note_off':
-        print("Turning off")
-        print("Turn Lights Off From Flask")
-        urllib.request.urlopen('http://localhost/18/off',data=None)
-        #os.system("chromium-browser http://localhost/18/off")
-        #webbrowser.open('http://localhost/18/off',new=0,autoraise=False)
     elif hasattr(msg, 'note') and msg.note == 84 and msg.type == 'note_on':
         print(msg)
         print("White Theater Chase")
-        urllib.request.urlopen('http://localhost/18/whiteTheaterChase',data=None)
-    elif hasattr(msg, 'note') and msg.note == 84 and msg.type == 'note_off':
+        urllib.request.urlopen('http://10.200.2.129/18/whiteTheaterChase',data=None)
+    elif hasattr(msg, 'note') and msg.note == 54 and msg.type == 'note_on':
         print(msg)
-        print("Clear RGB strip")
-        colorWipe(strip, Color(0, 0, 0), 10)
+        print("Red Green Alternate")
+        urllib.request.urlopen('http://10.200.2.129/18/redGreenAlternate',data=None)
+    elif hasattr(msg, 'note') and msg.note == 55 and msg.type == 'note_on':
+        print(msg)
+        print("Color range")
+        urllib.request.urlopen('http://10.200.2.129/18/redGreenAlternate',data=None)
+    elif hasattr(msg, 'note') and msg.note == 83 and msg.type == 'note_on':
+        print(msg)
+        print("Clear colors")
+        urllib.request.urlopen('http://10.200.2.129/18/off',data=None)
+   
     else:
         print(msg)
 if args.clear:
