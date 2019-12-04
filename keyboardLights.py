7745edbe127bb74b508fda4ae209ce79ebbe3e8c
austintedders13 <33536411+austintedders13@users.noreply.github.com>
2019-12-04 12:17:54 -0500

    Rearranged sound files, added wavs, full sounds on keyboard, adding more light effects

--- keyboardLights.py
+++ keyboardLights.py
@@ -25,140 +25,166 @@ for msg in port.__iter__():
    #in this instance we are checking for the note attribute to assign
    #the note name to the note itself, and only on note_on msg types
     if hasattr(msg, 'note') and msg.note == 53 and msg.type == 'note_on':
-        mixer.music.load('/home/pi/christmaslightproject/TubeBells.wav')
+        mixer.music.load('/home/pi/christmaslightproject/wavFiles/bellsF5.wav')
         mixer.music.play()
         print("Call Rainbow from Flask")
         urllib.request.urlopen('http://10.200.2.137/18/on',data=None)
         print(msg)
-    if hasattr(msg, 'note') and msg.note == 53 and msg.type == 'note_off':
-        mixer.music.stop()
-    
     if hasattr(msg, 'note') and msg.note == 54 and msg.type == 'note_on':
-        mixer.music.load('./fluteF6.wav')
+        mixer.music.load('./wavFiles/bellsGb5.wav')
         mixer.music.play()
         print(msg)
         print("Red Green Alternate")
         urllib.request.urlopen('http://10.200.2.137/18/redGreenAlternate',data=None)
-    if hasattr(msg, 'note') and msg.note == 54 and msg.type == 'note_off':
-        mixer.music.stop()
     if hasattr(msg, 'note') and msg.note == 55 and msg.type == 'note_on':
-        mixer.music.load('./SleighBells.wav')
+        mixer.music.load('./wavFiles/bellsG5.wav')
         mixer.music.play()
         print(msg)
         print("Color range")
         urllib.request.urlopen('http://10.200.2.137/18/theaterChaseRanger',data=None)
-    if hasattr(msg, 'note') and msg.note == 55 and msg.type == 'note_off':
-        mixer.music.stop() 
     if hasattr(msg, 'note') and msg.note == 56 and msg.type == 'note_on':
-        mixer.music.load('./fluteF6.wav')
-        mixer.music.play()
-        print(msg)
-        print("Red Green Alternate")
-        urllib.request.urlopen('http://10.200.2.137/18/Blue',data=None)
-    if hasattr(msg, 'note') and msg.note == 56 and msg.type == 'note_off':
-        mixer.music.stop()    
-    if hasattr(msg, 'note') and msg.note == 57 and msg.type == 'note_on':
-        mixer.music.load('./strings.wav')
+        mixer.music.load('./wavFiles/bellsAb5.wav')
         mixer.music.play()
         print(msg)
         print("Blue")
-        urllib.request.urlopen('http://10.200.2.137/18/rainbowTheaterChase',data=None)
-    if hasattr(msg, 'note') and msg.note == 57 and msg.type == 'note_off':
-        mixer.music.stop()
+        urllib.request.urlopen('http://10.200.2.137/18/Blue',data=None)
+    if hasattr(msg, 'note') and msg.note == 57 and msg.type == 'note_on':
+        mixer.music.load('./wavFiles/bellsA5.wav')
+        mixer.music.play()
+        print(msg)
+        print("Blue")
+        urllib.request.urlopen('http://10.200.2.137/18/3Colorbomb',data=None)
     if hasattr(msg, 'note') and msg.note == 58 and msg.type == 'note_on':
-        mixer.music.load('./strings.wav')
+        mixer.music.load('./wavFiles/bellsBb5.wav')
         mixer.music.play()
         print(msg)
         print("CC")
         urllib.request.urlopen('http://10.200.2.137/18/ccTheaterChase',data=None)
-    if hasattr(msg, 'note') and msg.note == 58 and msg.type == 'note_off':
-        mixer.music.stop()
     if hasattr(msg, 'note') and msg.note == 59 and msg.type == 'note_on':
-        mixer.music.load('./synthc5.wav')
+        mixer.music.load('./wavFiles/bellsB5.wav')
         mixer.music.play()
         print(msg)
         print("yellow blue")
-        urllib.request.urlopen('http://10.200.2.137/18/x',data=None)
-    if hasattr(msg, 'note') and msg.note == 59 and msg.type == 'note_off':
-        mixer.music.stop()           
+        urllib.request.urlopen('http://10.200.2.137/18/x',data=None)         
     if hasattr(msg, 'note') and msg.note == 60 and msg.type == 'note_on':
-        mixer.music.load('./synthc6.wav')
+        mixer.music.load('./wavFiles/bellsC6.wav')
         mixer.music.play()
         print(msg)
         print("Candy Cane")
         urllib.request.urlopen('http://10.200.2.137/18/candyCane',data=None)
-    if hasattr(msg, 'note') and msg.note == 60 and msg.type == 'note_off':
-        mixer.music.stop()
     if hasattr(msg, 'note') and msg.note == 61 and msg.type == 'note_on':
-        mixer.music.load('./synthc6.wav')
+        mixer.music.load('./wavFiles/bellsDb6.wav')
         mixer.music.play()
         print(msg)
         print("Red/Green/OrangeChase")
-        urllib.request.urlopen('http://10.200.2.137/18/theaterChase2',data=None)
-    if hasattr(msg, 'note') and msg.note == 61 and msg.type == 'note_off':
-        mixer.music.stop()         
+        urllib.request.urlopen('http://10.200.2.137/18/theaterChase2',data=None)         
     if hasattr(msg, 'note') and msg.note == 62 and msg.type == 'note_on':
-        mixer.music.load('./KawaiBellTree.wav')
+        mixer.music.load('./wavFiles/bellsD6.wav')
         mixer.music.play()
         print(msg)
-        print("White Theater Chase")
+        print("Purple Theater Chase")
         urllib.request.urlopen('http://10.200.2.137/18/purpleTheaterChase',data=None)
-    if hasattr(msg, 'note') and msg.note == 62 and msg.type == 'note_off':
-        mixer.music.stop()
     if hasattr(msg, 'note') and msg.note == 63 and msg.type == 'note_on':
-        mixer.music.load('./synthc6.wav')
+        mixer.music.load('./wavFiles/bellsEb6.wav')
         mixer.music.play()
         print(msg)
         print("TheaterChaseRange")
         urllib.request.urlopen('http://10.200.2.137/18/theaterChaseRanger',data=None)
-    if hasattr(msg, 'note') and msg.note == 63 and msg.type == 'note_off':
-        mixer.music.stop()
     if hasattr(msg, 'note') and msg.note == 64 and msg.type == 'note_on':
+        mixer.music.load('./wavFiles/bellsE6.wav')
+        mixer.music.play()
         print(msg)
         print("Silver and Gold")
         urllib.request.urlopen('http://10.200.2.137/18/silverAndGold',data=None)
     if hasattr(msg, 'note') and msg.note == 65 and msg.type == 'note_on':
-        mixer.music.load('./bellsA5.wav')
+        mixer.music.load('./wavFiles/bellsF6.wav')
         mixer.music.play()
         print(msg)
         print("OrangeRange")
         urllib.request.urlopen('http://10.200.2.137/18/orangeRange',data=None)
     if hasattr(msg, 'note') and msg.note == 66 and msg.type == 'note_on':
-        mixer.music.load('./bellsA5.wav')
+        mixer.music.load('./wavFiles/bellsGb6.wav')
         mixer.music.play()
         print(msg)
         print("Tree Test")
         urllib.request.urlopen('http://10.200.2.137/18/treeTest',data=None)
     if hasattr(msg, 'note') and msg.note == 67 and msg.type == 'note_on':
+        mixer.music.load('./wavFiles/bellsG6.wav')
+        mixer.music.play()
         print(msg)
         print("WhiteBushChase")
         urllib.request.urlopen('http://10.200.2.137/18/whiteBushChase')
     if hasattr(msg, 'note') and msg.note == 68 and msg.type == 'note_on':
+        mixer.music.load('./wavFiles/bellsAb6.wav')
+        mixer.music.play()
         print(msg)
         print("Bush 1 Green and Gold")
         urllib.request.urlopen('http://10.200.2.137/18/bushGreenGold')
     if hasattr(msg, 'note') and msg.note == 69 and msg.type == 'note_on':
+        mixer.music.load('./wavFiles/bellsA6.wav')
+        mixer.music.play()
         print(msg)
         print("Tree Combo CandyCane Chase")
         urllib.request.urlopen('http://10.200.2.137/18/treeComboChase')
     if hasattr(msg, 'note') and msg.note == 70 and msg.type == 'note_on':
+        mixer.music.load('./wavFiles/bellsBb6.wav')
+        mixer.music.play()
         print(msg)
         print("Pink and Blue Trees")
         urllib.request.urlopen('http://10.200.2.137/18/babyColors')
     if hasattr(msg, 'note') and msg.note == 71 and msg.type == 'note_on':
+        mixer.music.load('./wavFiles/bellsB6.wav')
+        mixer.music.play()
         print(msg)
         print("Whole Side Orange")
         urllib.request.urlopen('http://10.200.2.137/18/orangeSide')   
     if hasattr(msg, 'note') and msg.note == 72 and msg.type == 'note_on':
+        mixer.music.load('./wavFiles/bellsC7.wav')
+        mixer.music.play()
         print(msg)
         print("Red 2 Blue")
-        urllib.request.urlopen('http://10.200.2.137/18/red2Blue')                     
+        urllib.request.urlopen('http://10.200.2.137/18/red2Blue')     
+    if hasattr(msg, 'note') and msg.note == 73 and msg.type == 'note_on':
+        mixer.music.load('./wavFiles/bellsDb7.wav')
+        mixer.music.play()
+        print(msg)
+        print("New Action")
+        urllib.request.urlopen('http://10.200.2.137/18/red2Blue')
+    if hasattr(msg, 'note') and msg.note == 74 and msg.type == 'note_on':
+        mixer.music.load('./wavFiles/bellsD7.wav')
+        mixer.music.play()
+    if hasattr(msg, 'note') and msg.note == 75 and msg.type == 'note_on':
+        mixer.music.load('./wavFiles/bellsEb7.wav')
+        mixer.music.play()        
+    if hasattr(msg, 'note') and msg.note == 76 and msg.type == 'note_on':
+        mixer.music.load('./wavFiles/bellsE7.wav')
+        mixer.music.play()
+    if hasattr(msg, 'note') and msg.note == 77 and msg.type == 'note_on':
+        mixer.music.load('./wavFiles/bellsF7.wav')
+        mixer.music.play()                
+    if hasattr(msg, 'note') and msg.note == 78 and msg.type == 'note_on':
+        mixer.music.load('./wavFiles/bellsGb7.wav')
+        mixer.music.play()        
+    if hasattr(msg, 'note') and msg.note == 79 and msg.type == 'note_on':
+        mixer.music.load('./wavFiles/bellsG7.wav')
+        mixer.music.play()        
+    if hasattr(msg, 'note') and msg.note == 80 and msg.type == 'note_on':
+        mixer.music.load('./wavFiles/bellsAb7.wav')
+        mixer.music.play()        
+    if hasattr(msg, 'note') and msg.note == 81 and msg.type == 'note_on':
+        mixer.music.load('./wavFiles/bellsA7.wav')
+        mixer.music.play()        
+    if hasattr(msg, 'note') and msg.note == 82 and msg.type == 'note_on':
+        mixer.music.load('./wavFiles/bellsBb7.wav')
+        mixer.music.play()        
     if hasattr(msg, 'note') and msg.note == 83 and msg.type == 'note_on':
+        mixer.music.load('./wavFiles/bellsB7.wav')
+        mixer.music.play()  
         print(msg)
         print("Clear colors")
         urllib.request.urlopen('http://10.200.2.137/18/off',data=None)
     if hasattr(msg, 'note') and msg.note == 84 and msg.type == 'note_on':
-        mixer.music.load('./KawaiBellTree.wav')
+        mixer.music.load('./wavFiles/bellsC8.wav')
         mixer.music.play()
         print(msg)
         print("White Theater Chase")
